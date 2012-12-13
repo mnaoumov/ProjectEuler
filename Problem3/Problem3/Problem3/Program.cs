dbe5156fced85b9517431efaa65747803b7ed07a
Michael Naumov <michael.naumov@readify.net>
2012-12-13 22:51:52 +1100

    Add recommended solver for Problem3

--- Problem3/Problem3/Problem3/Program.cs
+++ Problem3/Problem3/Problem3/Program.cs
@@ -6,7 +6,7 @@ namespace Problem3
     {
         static void Main(string[] args)
         {
-            ISolver solver = new MySolver();
+            ISolver solver = new RecommendedSolver();
             Console.WriteLine(solver.Solve());
         }
     }
---
+++ Problem3/Problem3/Problem3/RecommendedSolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Problem3
+{
+    class RecommendedSolver : ISolver
+    {
+        public long Solve()
+        {
+            const long number = 600851475143;
+            return GetMaxPrimeFactor(number);
+        }
+
+        long GetMaxPrimeFactor(long number)
+        {
+            long lastFactor = 1;
+            while (number % 2 == 0)
+            {
+                lastFactor = 2;
+                number /= 2;
+            }
+            var factor = 3;
+            var maxFactor = Math.Sqrt(number);
+            while (number > 1 && factor <= maxFactor)
+            {
+                while (number % factor == 0)
+                {
+                    number /= factor;
+                    maxFactor = Math.Sqrt(number);
+                }
+
+                factor += 2;
+            }
+
+            return number == 1 ? lastFactor : number;
+        }
+    }
+}
