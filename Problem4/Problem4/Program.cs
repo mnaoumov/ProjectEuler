aaa5ef15eea1c8e5f9a0db537847ae8a47332951
Michael Naumov <michael.naumov@readify.net>
2012-12-14 08:32:25 +1100

    Add Problem4

---
+++ Problem4/Problem4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(My());
+        }
+
+        static int My()
+        {
+            var max = 0;
+            for (int i = 100; i <= 999; i++)
+            {
+                for (int j = 100; j <= 999; j++)
+                {
+                    var m = i * j;
+                    if (IsPalindrome(m) && m > max)
+                        max = m;
+                }
+            }
+
+            return max;
+        }
+
+        static bool IsPalindrome(int i)
+        {
+            var str = i.ToString();
+            var reversed = Reverse(str);
+
+            return str == reversed;
+        }
+
+        static string Reverse(string str)
+        {
+            return new string(str.Reverse().ToArray());
+        }
+    }
+}
