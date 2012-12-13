d642004ab2f749f03cebfc27d887de3a0d22e4e9
Michael Naumov <michael.naumov@readify.net>
2012-12-14 08:42:37 +1100

    Add Problem5

---
+++ Problem5/Problem5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyCommonMultiplier(20));
+        }
+
+        public static long MyCommonMultiplier(int n)
+        {
+            long m = 1;
+            for (int i = 1; i <= n; i++)
+                m = GCM(m, i);
+
+            return m;
+        }
+
+        static long GCM(long a, long b)
+        {
+            return a * b / GCD(a, b);
+        }
+
+        static long GCD(long a, long b)
+        {
+            while (true)
+            {
+                var r = a % b;
+                if (r == 0)
+                    return b;
+                
+                a = b;
+                b = r;
+            }
+        }
+    }
+}
---
+++ Problem5/Problem5/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Problem5
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void For_10_is_correct()
+        {
+            Assert.That(Program.MyCommonMultiplier(10), Is.EqualTo(2520));
+        }
+    }
+}
