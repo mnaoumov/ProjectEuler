df70ef91a2876b3b6eb6371cbc032bb0110add0c
Michael Naumov <michael.naumov@readify.net>
2012-12-18 22:54:01 +1100

    Problem7

---
+++ Problem7/Problem7/MySolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem7
+{
+    class MySolver
+    {
+        public long Solve(int n)
+        {
+            var index = 0;
+            var currentNumber = 1;
+            while (index < n)
+            {
+                currentNumber++;
+                if (IsPrime(currentNumber))
+                    index++;
+            }
+
+            return currentNumber;
+        }
+
+        static bool IsPrime(int n)
+        {
+            var maxDivisor = Math.Sqrt(n);
+
+            for (int i = 2; i <= maxDivisor; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Problem7/Problem7/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Problem7
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void MySolver_Solve_is_correct()
+        {
+            var solver = new MySolver();
+            Assert.That(solver.Solve(6), Is.EqualTo(13));
+        }
+    }
+}
