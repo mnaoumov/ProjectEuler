da6cfbba880dc621442a676fef96261d83919bcb
Michael Naumov <michael.naumov@readify.net>
2012-12-19 09:30:04 +1100

    Problem8

---
+++ Problem8/Problem8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new MySolver();
+            Console.WriteLine(solver.Solve());
+        }
+    }
+}
