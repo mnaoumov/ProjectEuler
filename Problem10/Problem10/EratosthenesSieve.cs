b5f8b3998f7a5bb0bad42198187030dacaa23b63
Michael Naumov <michael.naumov@readify.net>
2012-12-20 16:19:21 +1100

    Problem10

---
+++ Problem10/Problem10/EratosthenesSieve.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem10
+{
+    class EratosthenesSieve
+    {
+        public int[] GetPrimes(int max)
+        {
+            var primeCandidates = new bool[max + 1];
+            for (int i = 0; i < max + 1; i++)
+                primeCandidates[i] = true;
+
+            primeCandidates[0] = primeCandidates[1] = false;
+
+            var maxCandidate = Math.Sqrt(max);
+
+            for (int candidate = 2; candidate <= maxCandidate; candidate++)
+            {
+                if (!primeCandidates[candidate])
+                    continue;
+                for (int notPrime = candidate * 2; notPrime <= max; notPrime += candidate)
+                    primeCandidates[notPrime] = false;
+            }
+
+            return primeCandidates.Select((isPrime, number) => new { isPrime, number }).Where(x => x.isPrime).Select(x => x.number).ToArray();
+        }
+    }
+}
---
+++ Problem10/Problem10/MySolver.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Problem10
+{
+    class MySolver
+    {
+        public long Solve(int max)
+        {
+            var sieve = new EratosthenesSieve();
+            var primes = sieve.GetPrimes(max);
+            return primes.Select(x => (long) x).Sum();
+        }
+    }
+}
