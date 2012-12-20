b7f20c98f09cb102bd5c1b800aff3639b62691d6
Michael Naumov <michael.naumov@readify.net>
2012-12-21 10:02:19 +1100

    Add Problem11 with wrong solution - considered all possible neighbours

---
+++ Problem11/Problem11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new MySolver();
+            Console.WriteLine(solver.Solve());
+        }
+    }
+}
