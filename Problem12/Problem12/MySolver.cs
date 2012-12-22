e87f054ef46a8ae9be3da54fd6e96ec56cba585f
Michael Naumov <michael.naumov@readify.net>
2012-12-22 15:35:37 +1100

    Problem12

---
+++ Problem12/Problem12/MySolver.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem12
+{
+    class MySolver
+    {
+        public long Solve()
+        {
+            long n = 1;
+            while (true)
+            {
+                var triangleNumber = n * (n + 1) / 2;
+                var amountOfDivisors = GetAmountOfDivisors(triangleNumber);
+                if (amountOfDivisors >= 500)
+                    return triangleNumber;
+                n++;
+            }
+        }
+
+        int GetAmountOfDivisors(long number)
+        {
+            var divisor = 2;
+
+            var amount = 1;
+            while (divisor <= number)
+            {
+                var divisorPower = 0;
+                while (number % divisor == 0)
+                {
+                    divisorPower++;
+                    number /= divisor;
+                }
+                amount *= (divisorPower + 1);
+                divisor++;
+            }
+
+            return amount;
+        }
+    }
+}
