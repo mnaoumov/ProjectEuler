5c490d782a33258e4ddbae80239379f77e781566
Michael Naumov <michael.naumov@readify.net>
2012-12-26 10:56:05 +1100

    Problem15

---
+++ Problem15/Problem15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C_40^20
+
+            double result = 1;
+
+            for (int i = 0; i < 20; i++)
+            {
+                result *= 40 - i;
+                result /= 20 - i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
