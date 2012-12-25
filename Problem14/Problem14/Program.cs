eb4b60bead06c1a87a5e53f04d6b6346bcd18d1b
Michael Naumov <michael.naumov@readify.net>
2012-12-26 10:49:46 +1100

    Problem14

---
+++ Problem14/Problem14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Problem14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxChainNumber = 0;
+            var maxChainLength = 0;
+            for (int i = 1; i <= 1000000; i++)
+            {
+                var chainLength = GetChainLength(i);
+                if (chainLength > maxChainLength)
+                {
+                    maxChainLength = chainLength;
+                    maxChainNumber = i;
+                }
+            }
+
+            Console.WriteLine(maxChainNumber);
+        }
+
+        static int GetChainLength(int n)
+        {
+            long temp = n;
+            var chainLength = 1;
+            while (temp != 1)
+            {
+                if (temp % 2 == 0)
+                    temp /= 2;
+                else
+                    temp = 3 * temp + 1;
+                chainLength++;
+            }
+
+            return chainLength;
+        }
+    }
+}
