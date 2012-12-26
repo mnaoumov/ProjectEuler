e166da4ace14d49b7b4de02eb0fc6d7c2623b694
Michael Naumov <michael.naumov@readify.net>
2012-12-26 11:27:49 +1100

    Problem16

---
+++ Problem16/Problem16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pow = System.Numerics.BigInteger.Pow(2, 1000);
+            var sum = 0;
+            while (pow != 0)
+            {
+                var digit = (int) (pow % 10);
+                sum += digit;
+                pow /= 10;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
