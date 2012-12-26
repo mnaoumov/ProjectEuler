df7e8c801056bbee0cb85f8f6f4ba11a3387ab70
Michael Naumov <michael.naumov@readify.net>
2012-12-26 15:34:58 +1100

    Problem19

---
+++ Problem19/Problem19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            for (DateTime date = new DateTime(1901, 1, 1); date.Year <= 2000; date = date.AddMonths(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Sunday)
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
